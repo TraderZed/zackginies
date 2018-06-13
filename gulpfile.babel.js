ab067ce2c720cd2641151299b0080115c5fab3f9
Zack Ginies <zack@zackginies.com>
2018-06-13 16:55:02 -0400

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,44 @@
+"use strict";
+
+import gulp from "gulp";
+import haml from "gulp-haml";
+import sass from "gulp-sass";
+import terser from "gulp-terser";
+import autoprefixer from "gulp-autoprefixer";
+import imagemin from "gulp-imagemin";
+
+gulp.task("haml", () => {
+  return gulp
+    .src("./*.haml")
+    .pipe(haml())
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("es", () => {
+  return gulp
+    .src("src/assets/js/**/*.js")
+    .pipe(terser())
+    .pipe(gulp.dest("./public/assets/js/"));
+});
+
+gulp.task("styles", () => {
+  return gulp
+    .src("src/assets/sass/**/*.scss")
+    .pipe(autoprefixer())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./public/assets/css/"));
+});
+
+gulp.task("images", () => {
+  return gulp
+    .src("src/assets/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./public/assets/images/"));
+});
+
+//Watch task
+gulp.task("default", () => {
+  gulp.watch("./src/assets/sass/*.scss", ["styles"]);
+  gulp.watch("./src/assets/js/*.js", ["es"]);
+  gulp.watch("./*.haml", ["haml"]);
+});
